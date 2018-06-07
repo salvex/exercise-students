22b106388801b5a33ea08cd11c8f43751d776c64
Hawee <hawees@libero.it>
2018-06-07 18:41:00 +0200

    SETACCIO DI ERATOSTENE

--- DAWNIEL/Slide08_H04.c
+++ DAWNIEL/Slide08_H04.c
@@ -1,1 +1,44 @@
 /*IL SETACCIO DI ERATOSTENE*/
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 1000
+
+void inTo1( int s[], int n );
+void sieve( int s[], int n );
+void printRes( int s[], int n );
+
+int main( void ) {
+  int s[ SIZE ];
+  inTo1( s, SIZE );
+  sieve( s, SIZE );
+  printRes( s, SIZE );
+
+  return 0;
+}
+
+void inTo1( int s[], int n ) {
+  for (size_t i = 1; i < n; i++) {
+    s[ i ] = 1;
+  }
+}
+
+void sieve( int s[], int n ) {
+  for (size_t i = 2; i < n; i++) {
+    if ( s[i] == 1 ) {
+      for (size_t j = (i + 1); j < n; j++) {
+        if ( j % i == 0 ) {
+          s[j] = 0;
+        }
+      }
+    }
+
+  }
+}
+
+void printRes( int s[], int n ) {
+  for (size_t i = 0; i < n; i++) {
+    if (s[i] == 1) {
+      printf("%d is a prime number\n", i );
+    }
+  }
+}
